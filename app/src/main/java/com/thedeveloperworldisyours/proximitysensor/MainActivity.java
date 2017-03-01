5eb3580ec1c3d2de734157315b731584ba806063
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-03-01 11:42:13 +0000

    added design

--- app/src/main/java/com/thedeveloperworldisyours/proximitysensor/MainActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/proximitysensor/MainActivity.java
@@ -8,12 +8,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
